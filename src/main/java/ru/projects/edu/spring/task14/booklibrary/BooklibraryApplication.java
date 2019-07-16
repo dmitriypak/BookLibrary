e799307544890a4c23003f3a9a210c007c1f3cb9
Dmitriy Pak <fly2sky7777@mail.ru>
2019-07-16 23:03:39 +0300

    initial commit

---
+++ src/main/java/ru/projects/edu/spring/task14/booklibrary/BooklibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.projects.edu.spring.task14.booklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklibraryApplication.class, args);
+	}
+
+}
